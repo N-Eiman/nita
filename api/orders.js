91d5973126b04cd2414a33d91f1f4cad705cda0c
Thakane <122876128+N-Eiman@users.noreply.github.com>
2024-04-12 17:15:27 +0200

    [Feature]: created an interaction with postgres database(incomplete) with the .env and postgres.js files and added pool queries on the routes functions

--- api/orders.js
+++ api/orders.js
@@ -1,6 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
 
-
 //Orders are added through the post route
 let orders= [
   {
@@ -13,39 +12,57 @@ let orders= [
 
 //Get orders currently in the Database
 export const getOrders= (req, res) => {
-
-  res.send(orders);
-
+  pool.query('SELECT * FROM "Order"', (error, results) => {
+    if(error) {
+      throw error
+    }
+    res.send(orders);
+  })
   };
 
   //Filter through the available orders
   export const getOrdersById = (req, res) => {
     const { id } = req.params;
 
+    pool.query('SELECT * FROM "Order" WHERE id = ""',[id], (error, results) => {
+    if(error) {
+      throw error
+    }
     //To modify a specific order details, the unique Id for each posted order is used
     const foundClient = orders.find((order) => order.id === id);
 
     res.send(foundClient);
-  };
+  })
+};
 
 //Add an order to the Database
    export const createOrder = (req, res) => {
     const order = req.body;
-    
+
+    pool.query('SELECT * FROM "Order"', (error, results) => {
+    if(error) {
+      throw error
+    }
     //Every added order has a unique Id
     orders.push({ ...order, id: uuidv4()});
 
     res.send(`Order with Order ID: ${order.orderID} has been added to the DATABASE`);
-  };
+  })
+};
 
   //Remove orders from the Database using an id
   export const deleteOrder = (req, res) => {
     const { id } = req.params;
-    
+
+    pool.query('SELECT * FROM "Order" WHERE id = ""',[id], (error, results) => {
+    if(error) {
+      throw error
+    }
     orders = orders.filter((order) => order.id !== id);
 
     res.send(`Order with id: ${id} deleted from the DATABASE`);
-  };
+  })
+};
 
 
   //Update current orders
@@ -54,6 +71,11 @@ export const getOrders= (req, res) => {
     const { orderID, clientName, deliveryMethod } = req.body;
     const order = orders.find((order) => order.id === id);
     
+    pool.query('SELECT * FROM "Order"', (error, results) => {
+    if(error) {
+      throw error
+    }
+
     if (orderID) {
       order.orderID = orderID;
     };
@@ -67,5 +89,6 @@ export const getOrders= (req, res) => {
     };
 
     res.send(`Order with the ID: ${id} has been successfully updated!`);
-  };
+  })
+};
 
---
+++ api/utils/postgres.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+import pkg from "pg";
+dotenv.config()
+const { Pool } = pkg;
+
+export const pool = new pool({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DATABASE,
+    port: process.env.POSTGRES_PORT,
+});
